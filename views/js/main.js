066f445f0ff71a292ef9c015b341eda18f65fd33
JAMES E PURCELL <Jim@Jims-iMac.local>
2017-07-04 12:54:30 -0700

    Added a space between two lines to eliminate console error

--- views/js/main.js
+++ views/js/main.js
@@ -498,8 +498,11 @@ function logAverageFrame(times) {   // times is the array of User Timing measure
 
 // Moves the sliding background pizzas based on scroll position
 function updatePositions() {
+
   frame++;
+
   window.performance.mark("mark_start_frame");
+
   var items = document.getElementByClassName('mover');
 
   var length = items.length;
