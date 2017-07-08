c0f936576b6cbd7f9148ca49d81a49d3a0812ba2
JAMES E PURCELL <Jim@Jims-iMac.local>
2017-07-08 15:44:41 -0700

    Removed the phase calculation out of the for loop and produced a for loop to calculate phase 5 times

--- views/js/main.js
+++ views/js/main.js
@@ -497,9 +497,15 @@ function updatePositions() {
   frame++;
   window.performance.mark("mark_start_frame");
 
+  var phase = [];
+
+  for (i=0; i<5; i++) {
+    phase = Math.sin((document.body.scrollTop / 1250) + (i % 5));
+    phases.push(phase);
+  }
   var items = document.getElementsByClassName('mover');
   for (var i = 0; i < items.length; i++) {
-    var phase = Math.sin((document.body.scrollTop / 1250) + (i % 5));
+    // this is original var phase = Math.sin((document.body.scrollTop / 1250) + (i % 5));
     items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
   }
 
