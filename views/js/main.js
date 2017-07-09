ff640106c48640c076875afed75c0f58db5dcc37
JAMES E PURCELL <Jim@Jims-iMac.local>
2017-07-09 13:50:55 -0700

    Changed querySelectorAll to getElementByClassName on changePizzaSizes for loop

--- views/js/main.js
+++ views/js/main.js
@@ -445,7 +445,7 @@ var resizePizzas = function(size) {
 
   // Iterates through pizza elements on the page and changes their widths
   function changePizzaSizes(size) {
-    for (var i = 0; i < document.querySelectorAll(".randomPizzaContainer").length; i++) {
+    for (var i = 0; i < document.getElementById("randomPizzaContainer").length; i++) {
       var dx = determineDx(document.querySelectorAll(".randomPizzaContainer")[i], size);
       var newwidth = (document.querySelectorAll(".randomPizzaContainer")[i].offsetWidth + dx) + 'px';
       document.querySelectorAll(".randomPizzaContainer")[i].style.width = newwidth;
