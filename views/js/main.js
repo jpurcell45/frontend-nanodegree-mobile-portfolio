532037ef3934f6c8af42bd4a2c1c55118ba1fa53
JAMES E PURCELL <Jim@Jims-iMac.local>
2017-07-12 16:57:17 -0700

    Reduced the number of background pizzas to 20

--- views/js/main.js
+++ views/js/main.js
@@ -447,8 +447,8 @@ var resizePizzas = function(size) {
   function changePizzaSizes(size) {
     for (var i = 0; i < document.getElementsByClassName("randomPizzaContainer").length; i++) {
       //var dx = determineDx(document.querySelectorAll(".randomPizzaContainer")[i], size);
-      var newwidth = (document.getElementsByClassName(".randomPizzaContainer")[i].offsetWidth);
-      document.getElementsByClassName(".randomPizzaContainer")[i].style.width = newwidth;
+      var newwidth = (document.getElementsByClassName("randomPizzaContainer")[i].offsetWidth);
+      document.getElementsByClassName("randomPizzaContainer")[i].style.width = newwidth;
     }
   }
 
@@ -497,7 +497,7 @@ function updatePositions() {
   frame++;
   window.performance.mark("mark_start_frame");
 
-  var items = document.querySelectorAll('.mover');
+  var items = document.getElementsByClassName('mover');
   for (var i = 0; i < items.length; i++) {
     var phase = Math.sin((document.body.scrollTop / 1250) + (i % 5));
     items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
@@ -520,7 +520,7 @@ window.addEventListener('scroll', updatePositions);
 document.addEventListener('DOMContentLoaded', function() {
   var cols = 8;
   var s = 256;
-  for (var i = 0; i < 200; i++) {
+  for (var i = 0; i < 20; i++) {
     var elem = document.createElement('img');
     elem.className = 'mover';
     elem.src = "images/pizza.png";
