91fe564b2576cc02bb2a42a94d5472e0706ee591
Jim Purcell <pur.jim@gmail.com>
2017-07-15 09:54:23 -0700

    On updateposition function added items.length and phase in the for loop

--- views/js/main.js
+++ views/js/main.js
@@ -492,9 +492,8 @@ function updatePositions() {
   frame++;
   window.performance.mark("mark_start_frame");
   var scrollPosition = document.body.scrollTop / 1250;
-  for (var i = 0; i < items.length; i++) {
-    //var phase = Math.sin((document.body.scrollTop / 1250) + (i % 5));
-    var phase = Math.sin((scrollPosition) + (i %5));
+  for (var i = 0, len = items.length, phase; i < len; i++) {
+    var phase = Math.sin((scrollPosition) + (i % 5));
     items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
   }
 
